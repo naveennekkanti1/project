29792ec00d5803c9b7526667fe3d1b280b5a15f2
NaveenNekkanti <ndurganaveen11045@gmail.com>
2025-12-28 11:23:03 +0530

    first commit

--- api/_db.js
+++ api/_db.js
@@ -1,11 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://durganaveen:nekkanti@cluster0.8nibi9x.mongodb.net/fileapp?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://durganaveen:nekkanti@cluster0.8nibi9x.mongodb.net/fileApp?retryWrites=true&w=majority&appName=Cluster0";
+
+let client;
+let db;
 
 export async function connectDB() {
-  if (!client.topology?.isConnected()) {
+  if (!client) {
+    client = new MongoClient(uri);
     await client.connect();
+    db = client.db("fileApp");
   }
-  return client.db("fileApp");
+  return db;
 }
--- api/login.js
+++ api/login.js
@@ -1,14 +1,23 @@
 import { connectDB } from "./_db.js";
 
 export default async function handler(req, res) {
-  const db = await connectDB();
-  const { username, password } = JSON.parse(req.body);
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
-  const user = await db.collection("users").findOne({ username, password });
+  const body = JSON.parse(req.body);
+  const { username, password } = body;
+
+  const db = await connectDB();
+
+  const user = await db.collection("users").findOne({
+    username: username,
+    password: password
+  });
 
   if (!user) {
     return res.status(401).json({ message: "Invalid credentials" });
   }
 
-  res.status(200).json({ message: "success" });
+  res.status(200).json({ message: "Login success" });
 }
