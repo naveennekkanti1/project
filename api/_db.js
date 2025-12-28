c9145819ec02a51009e10a71b146345c888f1025
NaveenNekkanti <ndurganaveen11045@gmail.com>
2025-12-28 11:20:49 +0530

    first commit

--- api/_db.js
+++ api/_db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://durganaveen:nekkanti@cluster0.8nibi9x.mongodb.net/FileApp?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://durganaveen:nekkanti@cluster0.8nibi9x.mongodb.net/fileapp?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 export async function connectDB() {
