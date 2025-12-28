f778aa9c2ce942985469e6db208e59a817a64cba
NaveenNekkanti <ndurganaveen11045@gmail.com>
2025-12-28 11:13:19 +0530

    first commit

--- api/_db.js
+++ api/_db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI;
+const uri = "mongodb+srv://durganaveen:nekkanti@cluster0.8nibi9x.mongodb.net/FileApp?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 export async function connectDB() {
